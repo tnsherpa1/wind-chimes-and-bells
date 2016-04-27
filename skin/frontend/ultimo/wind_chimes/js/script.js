c472d246bf4b366e18988568e0a06fc5876dd443
tnsherpa1 <tnsherpa1@gmail.com>
2016-04-27 08:50:05 -0700

    plays mp3 audio when play icon is clicked

--- skin/frontend/ultimo/wind_chimes/js/script.js
+++ skin/frontend/ultimo/wind_chimes/js/script.js
@@ -1,6 +1,10 @@
 var $j = jQuery.noConflict();
 
 $j(document).ready(function() {
-	/*show and hide item no upon hover*/
-
+	/* Audio player on product grid layout */
+	$j('#audioControl').click(function(){
+		var audioSample = document.getElementById('ample');
+		audioSample.play();
+	});
 });
+
