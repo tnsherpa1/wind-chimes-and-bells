642225ef911300b59853436b3e5ea71459ed9c13
tnsherpa1 <tnsherpa1@gmail.com>
2016-04-27 12:19:09 -0700

    toggles play and pause button

--- skin/frontend/ultimo/wind_chimes/js/script.js
+++ skin/frontend/ultimo/wind_chimes/js/script.js
@@ -2,9 +2,18 @@ var $j = jQuery.noConflict();
 
 $j(document).ready(function() {
 	/* Audio player on product grid layout */
-	$j('#audioControl').click(function(){
+	$j('#audioControl').click(function() {
 		var audioSample = document.getElementById('ample');
-		audioSample.play();
+		if (audioSample.paused) {
+			$j("#audioControl").removeClass('fa-play-circle');
+			$j("#audioControl").addClass('fa-pause');
+			audioSample.play();
+		} 
+		else {
+			$j("#audioControl").removeClass('fa-pause');
+			$j("#audioControl").addClass('fa-play-circle');
+			audioSample.pause();
+		}
 	});
 });
 
